101fcac14087fbf09478e61cf75a9eb33a234d3a
Your Namesifathero <sifathero466@gmail.com>
2024-03-20 19:56:01 +0600

    looping

---
+++ looping.js
@@ -0,0 +1,21 @@
+const numbers =[2,3,4,5,76,7,6];
+for (const num of numbers){
+    console.log(num)
+};
+
+// string
+const string = ['rakib' ,'rifdat','imon','hashi'];
+for (const st of string){
+    console.log(st);
+}
+
+// object for in loop
+const glash ={
+    name: 'sanglas',
+    color:'black',
+    price:1200000,
+    isCleaned: true
+}
+for( const keys in glash){
+    console.log(keys);
+}
