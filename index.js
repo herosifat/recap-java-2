0ef704f4270c8cef113e2754d99c53c76b09b73f
Your Namesifathero <sifathero466@gmail.com>
2024-03-20 19:40:35 +0600

    recap-java-2

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const glash ={
     name: 'sanglas',
     color:'black',
     price:1200000,
-    isCleaned:true;
+    isCleaned: true
 }
 console.log(glash);
+const keys = Object.keys(glash);
+console.log(keys);
